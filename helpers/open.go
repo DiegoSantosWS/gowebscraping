fb84d58778cf45189a46caf2b98838019ac4d66e
DiegoSantosWS <tec.infor321@gmail.com>
2018-11-08 01:30:56 -0200

    add novo nome na estrutura

--- helpers/open.go
+++ helpers/open.go
@@ -12,13 +12,14 @@ import (
 var m = model.Connect{}
 
 // saveDatas save of data in collection
-func saveDatas(url, img, desc string) bool {
+func saveDatas(url, img, desc, data string) bool {
 
 	var d stru.DataColecteds
 	d.ID = bson.NewObjectId()
 	d.URL = url
 	d.Image = img
 	d.Description = desc
+	d.Data = data
 
 	if err := m.RegisterCollection("colects", d); err != nil {
 		log.Fatal(err)
