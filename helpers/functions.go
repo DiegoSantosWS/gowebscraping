69c137024f9a42edc944b3be1fd9109c23d2792e
DiegoSantosWS <tec.infor321@gmail.com>
2018-11-08 01:28:42 -0200

    creado novas functions para coletas

--- helpers/functions.go
+++ helpers/functions.go
@@ -35,16 +35,60 @@ func WsiteBrasilBlog() {
 			img, _ = s.Find("img").First().Attr("data-pagespeed-lazy-src")
 		}
 		txt := strings.TrimSpace(s.Find("h3").Text())
-		saveDatas(link, img, txt)
+		saveDatas(link, img, txt, "")
 	})
 }
 
 // UolNews perform a scraping on the website uol news
 func UolNews() {
+	resp, err := http.Get("https://economia.uol.com.br/ultimas/")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
 
+	if resp.StatusCode != 200 {
+		log.Fatalf("Status code error: %d %s", resp.StatusCode, resp.Status)
+	}
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	doc.Find(".thumbnail-standard").Each(func(i int, s *goquery.Selection) {
+		url, _ := s.Find("a").First().Attr("href")
+		img, _ := s.Find("img").First().Attr("src")
+		title := strings.TrimSpace(s.Find("h3").Text())
+		timer := s.Find("time").Text()
+
+		saveDatas(url, img, title, timer)
+	})
 }
 
-// GloboNews perform a scraping on the website g1
-func GloboNews() {
+// UolNews perform a scraping on the website uol news
+func ExameNews() {
+	resp, err := http.Get("https://exame.abril.com.br/noticias-sobre/mercado-financeiro/")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
 
+	if resp.StatusCode != 200 {
+		log.Fatalf("Status code error: %d %s", resp.StatusCode, resp.Status)
+	}
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	doc.Find(".list-item").Each(func(i int, s *goquery.Selection) {
+		url, _ := s.Find("a").First().Attr("href")
+		img, _ := s.Find("img").First().Attr("src")
+		title := strings.TrimSpace(s.Find(".list-item-title").Text())
+		timer := s.Find(".list-date-description").Text()
+
+		saveDatas(url, img, title, timer)
+	})
 }
