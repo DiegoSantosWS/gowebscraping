dfee10a0a01688bc9010a12444d9511314b906dd
DiegoSantosWS <tec.infor321@gmail.com>
2018-11-12 19:39:26 -0200

    add gocron

--- go.mod
+++ go.mod
@@ -1,7 +1,11 @@
 module github.com/DiegoSantosWS/gowebscraping
 
 require (
+	github.com/BurntSushi/toml v0.3.1
 	github.com/PuerkitoBio/goquery v1.4.1
 	github.com/andybalholm/cascadia v1.0.0 // indirect
+	github.com/globalsign/mgo v0.0.0-20181015135952-eeefdecb41b8
+	github.com/jasonlvhit/gocron v0.0.0-20180312192515-54194c9749d4
 	golang.org/x/net v0.0.0-20181106065722-10aee1819953 // indirect
+	gopkg.in/mgo.v2 v2.0.0-20180705113604-9856a29383ce
 )
--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/DiegoSantosWS/gowebscraping/config"
 	"github.com/DiegoSantosWS/gowebscraping/helpers"
 	"github.com/DiegoSantosWS/gowebscraping/model"
+	"github.com/jasonlvhit/gocron"
 )
 
 var dao = model.Connect{}
@@ -17,10 +20,24 @@ func init() {
 	dao.Connection()
 }
 
-func main() {
+func roundFunctions() {
 	helpers.WsiteBrasilBlog()
 	helpers.UolNews()
 	helpers.ExameNews()
 	helpers.UolEconomy()
 	helpers.InfoMoney()
+}
+func main() {
+	gocron.Every(1).Second().Do(helpers.WsiteBrasilBlog)
+	gocron.Every(1).Second().Do(helpers.UolNews)
+	gocron.Every(1).Second().Do(helpers.ExameNews)
+	gocron.Every(1).Second().Do(helpers.UolEconomy)
+	gocron.Every(1).Second().Do(helpers.InfoMoney)
+	_, time := gocron.NextRun()
+	fmt.Println(time)
+
+	//gocron.Remove(roundFunctions)
+	//gocron.Clear()
+
+	<-gocron.Start()
 }
